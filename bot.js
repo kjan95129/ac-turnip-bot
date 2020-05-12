718758e950a85a246aea7a66d0014d168baad421
Kevin Jan <kjan95129@gmail.com>
2020-05-11 18:20:15 -0700

    Added logic

--- bot.js
+++ bot.js
@@ -1,101 +1,59 @@
 // Run dotenv
 require("dotenv").config();
-const db = require("./db");
-const helpers = require("./helpers")
-
 const Discord = require("discord.js");
+var fs = require("fs");
 const client = new Discord.Client();
+let screamIntervalMap = {};
+
+const getRandomAudio = async () => {
+  // Randomize scream from /screams folder
+  let res = fs.readdir('./screams', function(err, files) {
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+    
+    const random = Math.floor(Math.random() * files.length);
+    screamFile = './screams/' + files[random];
+  })
+}
+
+// set default scream, rotate random scream every 5 seconds
+let screamFile = './screams/ahhh.m4a';
+screamFile = setInterval(getRandomAudio, 5000);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// msg.guild.id
-// msg.user.id
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+client.on("message", message => {
+  if (message.content === "!SCREAM") {
+    if (message.channel.type !== "text") return;
+
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) {
+      return message.reply("please join a voice channel first!");
+    }
+
+    // Logic to join, scream, and leave channel
+    const scream = () => {
+      voiceChannel.join().then(connection => {
+        const dispatcher = connection.play(screamFile);
+        dispatcher.on("finish", () => {
+          voiceChannel.leave();
+        });
+      });
+    };
+
+    // First scream before interval kicks off
+    scream();
+    let screamInterval = setInterval(scream, 20000);
+    screamIntervalMap[message.member.voice.channel.id] = screamInterval;
   }
 
-  if (msg.content.startsWith("!ac")) {
-    const commandArr = msg.content.split(" ");
-    let [ac, command, key, ...value] = commandArr
-    value = value.join(' ')
-    console.log(command, key, value);
-
-    if (command === "set") {
-      if (key === "friendcode") {
-        db.setCode(msg.guild.id, msg.author.username, value);
-        msg.reply("Set friendcode to " + value);
-      }
-      if (key === "fruit") {
-        db.setFruit(msg.guild.id, msg.author.username, value);
-        msg.reply("Set fruit to " + value);
-      }
-    }
-    else if (command === "get") {
-      if (key === "friendcode") {
-        db.getUser(msg.guild.id, value).then(code => {
-          if (code.data()) {
-            msg.reply(value + "'s friend code is " + code.data().code);
-          } else {
-            msg.reply(value + " hasn't set their friend code yet!");
-          }
-        });
-      }
-
-      if (key === "fruit") {
-        db.getUser(msg.guild.id, value).then(fruit => {
-          if (fruit.data()) {
-            msg.reply(value + "'s fruit is " + fruit.data().fruit);
-          } else {
-            msg.reply(value + " hasn't set their fruit yet!");
-          }
-        });
-      }
-
-      if (key === "turnip") {
-        if (value === "all") {
-          db.getDate(msg.guild.id, helpers.today).then(data => {
-            if(data.data()) {
-              msg.reply(helpers.sortByPrice(data.data()))
-            }
-            else {
-              msg.reply("No turnip prices have been recorded for today!");
-            }
-          });
-        } else {
-          db.getUser(msg.guild.id, value).then(data => {
-            if (data.data()) {
-              msg.reply(helpers.sortByTime(data.data(), value))
-            } else {
-              msg.reply(value + " hasn't recorded any turnip prices today!");
-            }
-          });
-        }
-      }
-    }
-    else if (command === "add") {
-      if (key === "turnip") {
-        db.addTurnipPrice(msg.guild.id, msg.author.username, value);
-        msg.reply("Added turnip price: " + value);
-      }
-    }
-    else {
-      helpMsg = `\nUse this bot to set/get friendcodes & fruit, and upload your turnip prices to the guild!
-      Commands: !ac help
-      Set Info: !ac set friendcode 1234-1234-1234, !ac set fruit pears
-      Get Info: !ac get friendcode Kevinipple, !ac get fruit Kevinipple
-      Add Turnip: !ac add turnip 100
-      Check Turnip Prices: !ac get turnip all
-      `
-
-      if(command !== "help") {
-        helpMsg = `Command not found.\n` + helpMsg
-      }
-
-      msg.reply(helpMsg)
-    }
+  if (message.content === "!STOP") {
+    clearInterval(screamIntervalMap[message.member.voice.channel.id]);
+    delete screamIntervalMap[message.member.voice.channel.id];
   }
 });
 
